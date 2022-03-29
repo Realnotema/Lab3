191890c9da9a00b6ec91b3e451e944b935ad7800
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-03-29 14:22:08 +0300

    Update process.c

--- process.c
+++ process.c
@@ -22,3 +22,59 @@ int DelTable (Table *table) {
         free(table);
         return 0;
 }
+
+int hash (int *hash_position) {
+        if (*hash_position == *hash_position % 10) return 1;
+                *hash_position %= 10;
+                return 0;
+}
+
+int AddElement (Table *table, int key1, int par, int key2, char *info) {
+        int pos1 = key1 - 1, pos2 = key2 - 1, flag = 0;
+        if (pos1 >= table->msize) {
+                pos1 = 0;      
+                while (table->ks1[pos1].busy == 1)      
+                        pos1++;
+        } 
+        if (pos1 < table->msize && table->ks1[pos1].busy == 1) {
+                while (pos1 != table->msize - 1 && table->ks1[pos1].busy == 1)
+                        pos1++;
+                if (table->ks1[pos1].busy == 1)
+                        flag++;
+                if (flag == 1) {
+                        for (int i = 0; i <  table->msize; i++) {
+                                if (table->ks1[i].busy == 0)
+                                        pos1 = i;
+                        }
+                }
+        }
+        if (flag == 1) {
+                char *errmess = "Memory error!";
+                puts(errmess);
+                return 1;
+        }
+        flag = 0;
+        if (pos2 >= table->msize) {
+                pos2 = 0;
+                while (table->ks2[pos2].busy == 1)
+                        pos2++;
+        }
+        while (pos2 < table->msize && table->ks2[pos2].busy == 1) {
+                flag = hash(&pos2);
+                for (int i = 0; i < table->msize && table->ks2[pos2].busy == 1; i++)
+                        pos2 = (pos2 + i) % table->msize;
+                if (flag == 1)
+                        break;
+        }
+        table->csize++;
+        table->ks1[pos1].key = key1;
+        table->ks1[pos1].busy = 1;
+        table->ks1[pos1].info = (Item *) malloc(sizeof(Item));
+        table->ks1[pos1].info->data = info;
+        table->ks1[pos1].info->realise = 0;
+        table->ks1[pos1].par = par;
+        table->ks2[pos2].key = key2;
+        table->ks2[pos2].busy = 1;
+        table->ks2[pos2].info = table->ks1[pos1].info;
+        return 0;
+}
