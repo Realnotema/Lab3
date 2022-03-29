45711348af039289a74cf822f41aedbc8b8c847e
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-03-29 20:23:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "process.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <readline/readline.h>
 
 int Dialog(const char *options[], int CountOptions) {
         int choose = 0;
@@ -11,40 +12,92 @@ int Dialog(const char *options[], int CountOptions) {
         char *errmess = "";
         do {
                 puts(errmess);
-                choose = GetInt();
+                do {
+                        choose = GetInt();
+                } while (choose > CountOptions || choose < 0);
                 if (choose < 0 || choose > CountOptions)
                         errmess = "Incorrect input. Try again";
         } while (choose < 0 || choose > CountOptions);
         return choose;
 }
 
-int Menu(int choose, Table *table) {
-        switch(choose) {
+int AddMenu (Table *table) {
+        int key[2];
+        int par;
+        char *mess[] = {"Enter first key:", "Enter second key ", "Enter parent of first element (0 if no parent)", "Enter string: "};
+        for (int i = 0; i < 2; i++) {
+                puts(mess[i]);
+                key[i] = GetInt();
+                if (i == 0) {
+                        puts(mess[2]);
+                        do {
+                                par = GetInt();
+                        } while (FindKey1(table, par) == 1 && par != 0);
+                }
+        }
+        puts(mess[3]);
+        char *string = readline("");
+        AddElement(table, key[0], par, key[1], string);
+        return 0;
+}
+
+int EditMenu (int choose, Table *table) {
+        char *mess[] = {"Enter key of parent:"};
+        switch (choose) {
                 case 1:
-                        printf("You choose 1. Add element\n");
+                        puts(mess[0]);
+                        int key = GetInt();
+                        DellWithChildren(table, key);
                         break;
                 case 2:
-                        printf("You choose 2. Edit element\n");
+                        printf("reorganize");
                         break;
                 case 3:
-                        printf("You choose 3. Delete element\n");
+                        printf("search");
                         break;
                 case 4:
-                        break;
-                default:
-                        exit (-1);
+                        return 1;
         }
+        return 0;
+}
+
+int Menu(int choose, Table *table) {
+        char *mess = "Enter key from first key space to delete:";
+        const char *options[] = {"1. Delete relatives of element", "2. Reorganize the table", "3. Search by version of element", "4. Quit"};
+        int flag;
+        switch(choose) {
+                case 1:
+                        AddMenu(table);
+                        break;
+                case 2:
+                        PrintTable(table);
+                        break;
+                case 3:
+                        puts(mess);
+                        int n = GetInt();
+                        DeleteElement(table, n);
+                        break;
+                case 4:
+                        do {
+                                flag = EditMenu(Dialog(options, 4), table);
+                        } while (flag == 0);
+                        break;
+                case 5:
+                        return 1;
+        }
+        return 0;
 }
 
 int main() {
-        const char *options[] = {"1. Add element", "2. Edit element", "3. Delete element", "4. Quit"};
+        int flag;
+        const char *options[] = {"1. Add element", "2. Print table", "3. Delete element", "4. Edit table", "5. Quit"};
         char *mess = "Enter count of keys:";
         puts(mess);
         int count = GetInt();
         Table *table = InitTable(count);
         do {
-                Menu(Dialog(options, 4), table);
-        } while (Dialog(options, 4) != 4);
+                flag = Menu(Dialog(options, 5), table);
+        } while (flag == 0);
         DelTable(table);
         return 0;
 }
