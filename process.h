0524f3bf50dfee82fd0c58fc410bc023b29c8e57
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-03-31 18:57:55 +0300

    Update process.h

--- process.h
+++ process.h
@@ -3,5 +3,12 @@
 
 Table *InitTable (int count);
 int DelTable (Table *table);
+int AddElement (Table *table, int key1, int par, int key2, int rel, char *info);
+int PrintTable (Table *table);
+int FindKey1 (Table *table, int key);
+int DeleteElement (Table *table, int key);
+int DellWithChildren (Table *table, int key);
+int Reorganize (Table *table);
+Table *SearchVersion (Table *table, int rel);
 
 #endif
